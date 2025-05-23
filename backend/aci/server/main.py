54efd840c585bc86ad37f004df37675cf1d2cb22
Usama Nadeem <143541989+usamanadeem786@users.noreply.github.com>
2025-05-24 04:44:42 +0500

    Update main.py

--- backend/aci/server/main.py
+++ backend/aci/server/main.py
@@ -98,7 +98,7 @@ app.add_middleware(SessionMiddleware, secret_key=config.SIGNING_KEY)
 # )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[config.DEV_PORTAL_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
