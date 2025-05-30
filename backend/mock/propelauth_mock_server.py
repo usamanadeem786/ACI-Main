fc348a743de42e94dabb5400f8f3f3a0c0dbee6e
Usama Nadeem <143541989+usamanadeem786@users.noreply.github.com>
2025-05-31 01:10:18 +0500

    Update propelauth_mock_server.py

--- backend/mock/propelauth_mock_server.py
+++ backend/mock/propelauth_mock_server.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://integrations-next.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
